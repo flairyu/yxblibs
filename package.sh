28139129920805a0963cef911ab5a5cb7ce9e4f9
Yu Xiangbo <flairyu@gmail.com>
2014-10-09 14:12:39 +0800

    用于Mac系统的压缩打包脚本

---
+++ package.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ $# -lt 1 ]; then
+cat<<HELP
+package -- zip a directory exclude .svn .git and .DS_Store.
+USAGE: package DIRICTORY
+HELP
+	exit 0
+fi
+
+echo $0
+echo $1
+
+#zip -r -9 $1.zip $1 -x *.svn* *.DS_Store* *.git*
+7za a -xr@/Users/yxb/bin/exfiles $1.7z $1
